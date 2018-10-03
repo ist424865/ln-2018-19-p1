0912a5fc814a1d9c34401531ced900d62c4c898d
Rui Ribeiro <rui.m.ribeiro@tecnico.ulisboa.pt>
2018-10-03 18:46:07 +0100

    Resolve exercise a

--- project/run.sh
+++ project/run.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 
-################### Exercicio a ################
-#
-# Compila e gera a versão gráfica do transdutor que tem "potato"
+### a)  misto2numerico
+
+# Cria o transdutor que converte de mmm para mm
 fstcompile --isymbols=syms.txt --osymbols=syms-out.txt  mmm2mm.txt | fstarcsort > mmm2mm.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait mmm2mm.fst | dot -Tpdf  > mmm2mm.pdf
 
+# Cria o transdutor que processa numeros e '/'
 fstcompile --isymbols=syms.txt --osymbols=syms-out.txt  numerico2numerico.txt | fstarcsort > numerico2numerico.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait numerico2numerico.fst | dot -Tpdf  > numerico2numerico.pdf
 
-################### Testa os tradutores ################
-#
-# Compila e gera a versão gráfica do transdutor que traduz Inglês em Português
-fstconcat numerico2numerico.fst mmm2mm.fst > misto2numerico.fst
+# Cria o transdutor que converte uma data do formato condensado misto para o numérico
+fstconcat numerico2numerico.fst mmm2mm.fst > misto2numerico_aux.fst
+fstconcat misto2numerico_aux.fst numerico2numerico.fst > misto2numerico.fst
 fstdraw --isymbols=syms.txt --osymbols=syms-out.txt  --portrait misto2numerico.fst | dot -Tpdf > misto2numerico.pdf
+
